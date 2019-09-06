c9580d0d0a4100195ca65e18092e660b8041fc67
PiZep <pizep@outlook.com>
2019-09-06 17:35:29 +0200

    venv install completed

--- install_venv.sh
+++ install_venv.sh
@@ -16,24 +16,27 @@ DISTRIB=$(lsb_release -is)          # distribution linux
 # des paquets python3 via pip3
 if [ $# -lt 1 ]
 then
-	PIP_REQ="requirements.txt"
+    PIP_REQ="requirements.txt"
 else
-	PIP_REQ="requirements-$1.txt"
+    PIP_REQ="requirements-$1.txt"
+    
+    if [ $1 = "opencv" ]
+    then
+	# Installation par gestionnaire de paquets d'opencv et numpy.
+	# L'installation par pip3 ne fonctionne pas pour python 3.6-7.
+	case $DISTRIB in
+	    # VoidLinux : machine de test
+	    "VoidLinux")
+	    sudo xbps-install -S libopencv-python3
+	    ;;
+	"Debian" | "Raspbian")
+	    sudo apt install python3-opencv -y
+	    ;;
+	esac
+    fi
 fi
 
 echo $WPATH
-
-# Installation par gestionnaire de paquets d'opencv et numpy.
-# L'installation par pip3 ne fonctionne pas pour python 3.6-7.
-case $DISTRIB in
-    # VoidLinux : machine de test
-	"VoidLinux")
-		sudo xbps-install -S libopencv-python3
-		;;
-	"Debian" | "Raspbian")
-		sudo apt install python3-opencv -y
-		;;
-esac
 
 # Installation de l'environnement virtuel
 python3 -m venv $WPATH/env/
@@ -44,15 +47,17 @@ PYTHON_VER="python3."$(python3 -c 'import sys; print(sys.version_info[1])')
 echo $PYTHON_VER
 
 pip3 install -r $PIP_REQ
-
 deactivate
+
 # Création des liens des librairies installées sur le système
-# vers l'environnement virtuel
-CV2_LIB=$(python3 -c 'import cv2; print(cv2.__file__)')
-ln -s $CV2_LIB $WPATH/env/lib/$PYTHON_VER/site-packages/
+# vers l'environnement virtuel pour opencv
+if [ $# -lt 1 -a $1 = "opencv" ]
+then
+    CV2_LIB=$(python3 -c 'import cv2; print(cv2.__file__)')
+    ln -s $CV2_LIB $WPATH/env/lib/$PYTHON_VER/site-packages/
 
-NUMPY_LIB=$(python3 -c 'import numpy; print(numpy.__path__)')
-NUMPY_LIB=${NUMPY_LIB%"']"}
-NUMPY_LIB=${NUMPY_LIB#"['"}
-ln -s $NUMPY_LIB $WPATH/env/lib/$PYTHON_VER/site-packages/
-
+    NUMPY_LIB=$(python3 -c 'import numpy; print(numpy.__path__)')
+    NUMPY_LIB=${NUMPY_LIB%"']"}
+    NUMPY_LIB=${NUMPY_LIB#"['"}
+    ln -s $NUMPY_LIB $WPATH/env/lib/$PYTHON_VER/site-packages/
+fi
