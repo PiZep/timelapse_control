4674e104aec595d0d323728f427d135480a59468
PiZep <pizep@outlook.com>
2019-09-11 23:52:58 +0200

    working on config

--- timelapse.py
+++ timelapse.py
@@ -22,45 +22,31 @@ START_HOUR = tlconfig.START_HOUR
 START_MIN = tlconfig.START_MIN
 END_HOUR = tlconfig.END_HOUR
 END_MIN = tlconfig.END_MIN
-LAST_PIC = tlconfig.LAST
+LAST_PIC = tlconfig.LAST_PIC
 
 
-class TimeLapse(object):
-    """"""
+class TimeLapse():
+    """Main TimeLapse class"""
 
     def __init__(self, camera):
-        camera
         self.camera = camera
         self.config = tlconfig
         self.last_pic = None
         self.last_shot = None
         self._count = 0
 
-    def name_picture(self):
-        """Set and return the name of a picture"""
-        print("name_picture")
-        return tlconfig.PATH
-
-    def take_picture(self, name_fun=None, *args, **kwargs):
+    def take_picture(self, name_fun, *args, **kwargs):
         """Take a picture"""
         print("take_picture")
         self._count += 1
         if name_fun:
             name = name_fun(*args, **kwargs)
         else:
-            name = self._count
+            name = str(self._count) + ".jpg"
         pic_full_path = os.path.sep.join((PATH, name))
         print(pic_full_path)
         self.camera.take_picture(pic_full_path, CAM_RES)
         return name
-
-    def daysset():
-        """Check that every days are set the same
-
-        All values True or False means every day is set
-        """
-        print("daysset")
-        return DAYS[1:] == DAYS[:-1]
 
     def delay(self):
         """Calculate the delay in second before the next picture"""
@@ -79,10 +65,9 @@ class TimeLapse(object):
         d_days = 0
         if forecast > limit:
             d_days += 1
-            if not self.alldays:
-                while not DAYS[week_day]:
-                    week_day = week_day + 1 if week_day < 6 else 0
-                    d_days += 1
+            while not DAYS[week_day]:
+                week_day = week_day + 1 if week_day < 6 else 0
+                d_days += 1
 
             next_pic = datetime(year=self.last_shot.year, month=next_pic.month,
                                 day=self.last_shot.day + d_days,
@@ -94,14 +79,15 @@ class TimeLapse(object):
         print(d_days, delay.total_seconds(), self.last_shot, next_pic)
         return delay.total_seconds()
 
-    def timelapse(self):
+    def timelapse(self, name_func, *args, **kwargs):
         """Set timelapse"""
         print("timelapse")
         while True:
             self.last_shot = time.time()
-            self.last_pic = self.take_picture()
+            self.last_pic = self.take_picture(name_func, *args, **kwargs)
             if TIMESET:
                 time.sleep(self.delay())
             else:
-                time.sleep(INTERVAL - time.time() - self.last_shot)
+                time.sleep(INTERVAL - self.last_shot)
+            return self.last_shot
 
--- tlconfig.py
+++ tlconfig.py
@@ -3,52 +3,85 @@
 
 """Main class of the application"""
 import os
-# import threading
-# from camera_opencv import Camera
 import json
 
 # camera = Camera()
-default = {"timeset": False,
+DEFAULT = {"timeset": False,
            "res": {"width": 320, "height": 240},
            "interval": 60,
            "path": os.getcwd(),
            "days": [False] * 7,
-           "start": {"hour": 0, "min": 0},
-           "end": {"hour": 0, "min": 0}
+           "start": {"hour": 0, "minute": 0},
+           "end": {"hour": 0, "minute": 0},
+           "lastpic": None
            }
-param = {}
 
-try:
-    with open('timelapse.json', 'r') as conf:
-        param = json.load(conf)
+PARAM = {}
 
-        TIMESET = param['timeset']
-        CAM_RES = (param['res']['width'], param['res']['height'])
-        INTERVAL = param['interval']
-        PATH = param['path']
-        DAYS = param['days']
-        START_HOUR = param['start']['hour']
-        START_MIN = param['start']['minute']
-        END_HOUR = param['end']['hour']
-        END_MIN = param['end']['minute']
-        LAST_PIC = param['lastpic']
-except FileNotFoundError:
-    with open('timelapse.json', 'w') as conf:
-        param = default
-        json.dump(default, conf)
+TIMESET = DEFAULT['timeset']
+CAM_RES = (DEFAULT['res']['width'], DEFAULT['res']['height'])
+INTERVAL = DEFAULT['interval']
+PATH = DEFAULT['path']
+DAYS = DEFAULT['days']
+START_HOUR = DEFAULT['start']['hour']
+START_MIN = DEFAULT['start']['minute']
+END_HOUR = DEFAULT['end']['hour']
+END_MIN = DEFAULT['end']['minute']
+LAST_PIC = DEFAULT['lastpic']
+
+
+def check_path(path, addeddir=''):
+    """Check if the path exists, else return working dir"""
+    if not (os.path.isdir(path) and os.access(path, os.F_OK)):
+        path = os.getcwd()
+
+    if len(addeddir.split(os.path.sep)) >= 1:
+        split_dir = addeddir.split(os.path.sep).insert(0, path)
+        path = os.path.sep.join(split_dir)
+        os.makedirs(path)
+
+    return path
 
 
 def get_config():
     """Get the actual configuration"""
-    return param
+    global PARAM
+    try:
+        with open('timelapse.json', 'r') as conf:
+            PARAM = json.load(conf)
+            PARAM['PATH'] = check_path(PATH)
+            set_config(PARAM)
+    except FileNotFoundError:
+        with open('timelapse.json', 'w') as conf:
+            PARAM = DEFAULT
 
 
 def set_config(new_param):
     """Write back new parameters"""
+    global TIMESET
+    global CAM_RES
+    global INTERVAL
+    global PATH
+    global DAYS
+    global START_HOUR
+    global START_MIN
+    global END_HOUR
+    global END_MIN
+    global LAST_PIC
     with open('timelapse.json', 'w') as conf:
+        TIMESET = new_param['timeset']
+        CAM_RES = (new_param['res']['width'], new_param['res']['height'])
+        INTERVAL = new_param['interval']
+        PATH = check_path(new_param['path'])
+        DAYS = new_param['days']
+        START_HOUR = new_param['start']['hour']
+        START_MIN = new_param['start']['minute']
+        END_HOUR = new_param['end']['ur']
+        END_MIN = new_param['end']['mute']
+        LAST_PIC = new_param['lastpic']
         json.dump(new_param, conf)
 
 
 if __name__ == "__main__":
-    set_config()
+    set_config(PARAM)
 
