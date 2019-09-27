ea1e8d211224fe7cd0f8c1fde670da2938fa986b
PiZep <pizep@outlook.com>
2019-09-27 14:45:26 +0200

    Working on the UI

--- static/js/tlform.js
+++ static/js/tlform.js
@@ -1,4 +1,19 @@
 
-      function convertToImgPerMin(interval) {
-        document.querySelector("#freq").innerHTML = 60/interval;
-      }
+window.onload = function(event) {
+  convertToImgPerMin(document.getElementById('interval').value);
+  showOrHide('block');
+}
+
+function convertToImgPerMin(interval) {
+  document.querySelector("#freq").innerHTML = 60/interval;
+}
+
+function showOrHide(block) {
+  var checkBox = document.getElementById("days");
+  if (checkBox.checked == true) {
+    visibility = "block";
+  } else {
+    visibility = "none";
+  }
+  document.getElementById(block).style.display = visibility;
+}
